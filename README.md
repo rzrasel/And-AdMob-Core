5dc4f40c8d561d62876e74138271ee603e2c625d
Rz Rasel <Rz Rasel>
2017-12-10 21:45:43 +0600

    Add Project

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # And-AdMob-Core
 And AdMob Core
+
+### GIT Commit
+```git_commit
+git init
+git remote add origin https://github.com/rzrasel/And-AdMob-Core.git
+git remote -v
+git fetch && git checkout master
+git add .
+git commit -m "Add Library Project File"
+git pull
+git push --all
+```
---
+++ admobcore/src/main/java/com/rz/admobcore/admob/AdMobBanner.java
@@ -0,0 +1,67 @@
+package com.rz.admobcore.admob;
+
+import android.app.Activity;
+import android.content.Context;
+import android.provider.Settings;
+
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
+
+/**
+ * Created by Rz Rasel 2017-11-20.
+ */
+
+public class AdMobBanner {
+    private Activity activity;
+    private Context context;
+    private AdView adView;
+    private static AdMobBanner instance = null;
+
+    public static AdMobBanner getInstance(Activity argActivity, Context argContext) {
+        if (instance == null) {
+            instance = new AdMobBanner(argActivity, argContext);
+        }
+        return instance;
+    }
+
+    public AdMobBanner(Activity argActivity, Context argContext) {
+        this.activity = argActivity;
+        this.context = argContext;
+    }
+
+    /**
+     * Does some thing in old style.
+     *
+     * @deprecated use {@link # new()} instead.
+     */
+    @Deprecated
+    public void onInit(int argViewId) {
+        String androidId = Settings.Secure.getString(activity.getContentResolver(), Settings.Secure.ANDROID_ID);
+        String testDeviceId = MD5Builder.md5(androidId).toUpperCase();
+        //adView = (AdView) activity.findViewById(R.id.adView);
+        //adView = argViewId;
+        /*AdRequest adRequest = new AdRequest.Builder().build();
+        adRequest.addTestDevice(deviceId);*/
+        AdRequest adRequest = new AdRequest.Builder()
+                .addTestDevice(AdRequest.DEVICE_ID_EMULATOR)
+                .addTestDevice(testDeviceId)
+                .build();
+        adView.loadAd(adRequest);
+    }
+
+    public void onInit(AdView argAdView) {
+        String androidId = Settings.Secure.getString(activity.getContentResolver(), Settings.Secure.ANDROID_ID);
+        String testDeviceId = MD5Builder.md5(androidId).toUpperCase();
+        //LogWriter.Log("TEST_DEVICE_ID: " + testDeviceId);
+        testDeviceId = "C148757B5D2F4FF25294A0A2CA699D76";
+        //adView = (AdView) activity.findViewById(R.id.adView);
+        this.adView = argAdView;
+        /*AdRequest adRequest = new AdRequest.Builder().build();
+        adRequest.addTestDevice(deviceId);*/
+        AdRequest adRequest = new AdRequest.Builder()
+                .addTestDevice(AdRequest.DEVICE_ID_EMULATOR)
+                .addTestDevice(testDeviceId)
+                .build();
+        adView.loadAd(adRequest);
+    }
+}
